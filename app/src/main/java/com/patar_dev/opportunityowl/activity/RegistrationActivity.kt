c162e6b02a1539709fdf49d2097d93c079d23687
Devajit <patar1447@gmail.com>
2023-10-06 13:37:22 +0530

    Profile Image picker feature implemented at Registration Activity

--- app/src/main/java/com/patar_dev/opportunityowl/activity/RegistrationActivity.kt
+++ app/src/main/java/com/patar_dev/opportunityowl/activity/RegistrationActivity.kt
@@ -1,17 +1,32 @@
 package com.patar_dev.opportunityowl.activity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.patar_dev.opportunityowl.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
     private lateinit var binding:ActivityRegistrationBinding
+
+    private var imageUri:Uri?=null
+    private var selectImage=registerForActivityResult(ActivityResultContracts.GetContent()){
+        imageUri=it
+
+        binding.userImage.setImageURI(imageUri)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //Pick Profile Image
+        binding.userImage.setOnClickListener {
+            selectImage.launch("image/*")
+        }
 
         //Intent to Login Page
         binding.txtLogin.setOnClickListener {
