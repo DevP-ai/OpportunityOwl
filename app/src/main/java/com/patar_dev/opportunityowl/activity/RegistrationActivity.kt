d8c2e497766894c0e77a3112c11e52c652f1b9b5
Devajit <patar1447@gmail.com>
2023-10-06 13:22:25 +0530

    Add Functionality to go back Login Page from Registration page

--- app/src/main/java/com/patar_dev/opportunityowl/activity/RegistrationActivity.kt
+++ app/src/main/java/com/patar_dev/opportunityowl/activity/RegistrationActivity.kt
@@ -1,12 +1,22 @@
 package com.patar_dev.opportunityowl.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.patar_dev.opportunityowl.R
+import com.patar_dev.opportunityowl.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding=ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Intent to Login Page
+        binding.txtLogin.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        
     }
 }
