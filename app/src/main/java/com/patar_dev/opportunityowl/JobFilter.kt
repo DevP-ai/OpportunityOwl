dacf0bdfb98477a0ab65cea4f7afc5fe0bb3a1b2
Viraj76 <virajg.7680@gmail.com>
2023-10-08 15:52:54 +0530

    Filter class for filtering jobs post

---
+++ app/src/main/java/com/patar_dev/opportunityowl/JobFilter.kt
@@ -0,0 +1,49 @@
+package com.patar_dev.opportunityowl
+
+import android.widget.Filter
+import com.patar_dev.opportunityowl.adapter.JobListAdapter
+import com.patar_dev.opportunityowl.model.job.JobModel
+import java.util.Locale
+
+class JobFilter(
+    private val adapter: JobListAdapter,
+    private val filterJobs : ArrayList<JobModel>
+) : Filter() {
+    override fun performFiltering(searchingText: CharSequence?): FilterResults {
+        val filteredResults = FilterResults()
+
+        if(!searchingText.isNullOrEmpty()){
+            val query = searchingText.toString().trim().uppercase(Locale.getDefault()).split(" ")
+            val filteredJobList = ArrayList<JobModel>()
+            for(jobs in filterJobs){
+
+                if(query.any { search ->
+                        jobs.companyName?.uppercase(Locale.getDefault())?.contains(search) == true ||
+                                jobs.jobTitle?.uppercase(Locale.getDefault())?.contains(search) == true ||
+                                jobs.location?.uppercase(Locale.getDefault())?.contains(search) == true
+
+                    }){
+                    filteredJobList.add(jobs)
+                }
+            }
+            filteredResults.apply {
+                count = filteredJobList.size
+                values = filteredJobList
+            }
+
+        }
+        else{
+            filteredResults.apply {
+                count = filterJobs.size
+                values = filterJobs
+            }
+        }
+
+        return filteredResults
+    }
+
+    override fun publishResults(p0: CharSequence?, results: FilterResults?) {
+        adapter.jobList = results?.values as ArrayList<JobModel>
+        adapter.notifyDataSetChanged()
+    }
+}
